69e0d5bdcddc5b998f20d6d36ac61fe617ddbad6
ghadeeras <ghadeer.abousaleh@gmail.com>
2021-06-04 18:36:47 -0400

    Fix for example.sh script

--- example.sh
+++ example.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-set MODULE_PATH="./build/libs/javafx-base-14.jar:./build/libs/javafx-base-14-linux.jar:./build/libs/javafx-graphics-14-linux.jar"
-$JAVA_HOME/bin/java --module-path=$MODULE_PATH --add-modules=javafx.graphics -cp "${MODULE_PATH}:./libs/vibrato-1.0-SNAPSHOT.jar:./build/classes/java/test" vibrato.examples.$@
+MODULE_PATH="./build/libs/javafx-base-14.jar:./build/libs/javafx-base-14-linux.jar:./build/libs/javafx-graphics-14-linux.jar"
+"$JAVA_HOME/bin/java" --module-path=$MODULE_PATH --add-modules=javafx.graphics -cp "${MODULE_PATH}:./build/libs/vibrato-1.0-SNAPSHOT.jar:./build/classes/java/test" "vibrato.examples.$1"
